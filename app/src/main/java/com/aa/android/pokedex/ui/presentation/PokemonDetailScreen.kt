818d60bb35cba5258d321eeda1f4d2a5e79608de
hojat72elect <hojat72elect@gmail.com>
2024-11-15 20:29:57 -0500

    Added dummy UI for PokemonDetailScreen.

--- app/src/main/java/com/aa/android/pokedex/ui/presentation/PokemonDetailScreen.kt
+++ app/src/main/java/com/aa/android/pokedex/ui/presentation/PokemonDetailScreen.kt
@@ -1,12 +1,23 @@
 package com.aa.android.pokedex.ui.presentation
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
 import com.aa.android.pokedex.viewmodel.DetailsViewModel
 
 @Composable
@@ -19,7 +30,44 @@ fun PokemonDetailScreen(viewModel: DetailsViewModel, pokemonName: String) {
  */
 @Composable
 private fun PokemonDetailScreen(pokemon: String) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = pokemon, fontSize = 24.sp)
+
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(scrollState),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = rememberImagePainter("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg"),
+            contentDescription = "Squirtle",
+            modifier = Modifier
+                .size(200.dp)
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(18.dp))
+        Text(
+            modifier = Modifier.padding(vertical = 8.dp),
+            text = "Squirtle",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.Start
+        ) {
+
+            Text("Height :  5", modifier = Modifier.padding(vertical = 8.dp))
+            Text("Weight :  90", modifier = Modifier.padding(vertical = 8.dp))
+            Text("Type :  Water", modifier = Modifier.padding(vertical = 8.dp))
+
+            // A list of these:
+            Text("hp : 44 - effort points : 0", modifier = Modifier.padding(vertical = 8.dp))
+
+        }
+
     }
 }
